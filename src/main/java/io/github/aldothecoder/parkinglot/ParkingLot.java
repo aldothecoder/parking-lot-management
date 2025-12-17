51bec5611f77806d7e36bce0b5ec4ac6f3cc781c
aldothecoder <aldodiazfitness@gmail.com>
2025-12-17 10:45:05 -0600

    implement parking lot slot tracking

--- src/main/java/io/github/aldothecoder/parkinglot/ParkingLot.java
+++ src/main/java/io/github/aldothecoder/parkinglot/ParkingLot.java
@@ -5,6 +5,21 @@ import java.util.Map;
 
 public class ParkingLot {
 
+    private final Map<SlotType, Integer> totalSlots;
+    private final Map<SlotType, Integer> availableSlots;
+
+    public ParkingLot(int small, int large, int oversize){
+
+        totalSlots = new EnumMap<>(SlotType.class);
+        availableSlots = new EnumMap<>(SlotType.class);
+
+        totalSlots.put(SlotType.SMALL, small);
+        totalSlots.put(SlotType.LARGE, large);
+        totalSlots.put(SlotType.OVERSIZE, oversize);
+
+        availableSlots.putAll(totalSlots);
+
+    }
 
 
 }
